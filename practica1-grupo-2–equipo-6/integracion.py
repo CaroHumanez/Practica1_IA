0ea82c16a5fe0d96443cdba3dbac25d54655e66e
Diego-Fernando-Juajibioy <74009998+Diego-Fernando-Juajibioy@users.noreply.github.com>
2025-05-12 13:51:20 -0500

    Removed old files with incorrect folder name encoding

--- practica1-grupo-2–equipo-6/integracion.py
+++
@@ -1,6 +0,0 @@
-from logicaDifusa import cargar_recomendabilidad
-
-#aqui se optiene la recomendabilidad dese la lofica difusa
-r=cargar_recomendabilidad(interes,apertura,edad)
-
-#PEGAR AQUI LA INTERFAZ
